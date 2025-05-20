f28670bdea692f34a742c1c6462d148aa95216c1
Khao0 <115398809+Khao0@users.noreply.github.com>
2025-05-20 12:49:12 +0700

    sigmoid function

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,10 @@ def softmax(xv):
     e_x = np.power(np.e, xv-np.max(xv)) # subtract for preventing from high computation of exponent high value
     return e_x/np.sum(e_x)
 
+def sigmoid(xv):
+    return 1/(1+np.exp(-xv))
+
 
 if __name__ =="__main__":
-    result = softmax(np.array([0.8,0.2]))
+    result = sigmoid(np.array([0.8,0.2]))
     print(result)
