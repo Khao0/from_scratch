af52ba1723dc6a13b50b94daa7fe56c063607532
Khao0 <115398809+Khao0@users.noreply.github.com>
2026-01-30 19:01:09 +0700

    finish h1

--- TraditionalAI/SearchProblems/search.py
+++ TraditionalAI/SearchProblems/search.py
@@ -21,8 +21,7 @@ class Node:
     distance:int
     heuristic:int
     is_ZT_used:bool
-    parent: "Node | None" = None # for path reconstruction
-
+    # parent: "Node | None" = None # for path reconstruction and debugging
 
     def __post_init__(self):
         self.priority = self.distance + self.heuristic
@@ -30,11 +29,14 @@ class Node:
     def is_goal(self, goal:Position) -> bool:
         return self.position == goal
     
+    def get_key(self) -> Tuple[Position, bool]:
+        return (self.position, self.is_ZT_used)
+    
 # heapq for priority queue with time complexity O(log n)
 
 
 class Search:
-    SUCCESSORS : List[Position] = [
+    SUCCESSORS : List[Action] = [
         (0, 1),
         (-1, 0),
         (0, -1),
@@ -46,21 +48,24 @@ class Search:
         Initializes the Search class.
         """
         self.fringe:List[Position] = list()
-        self.explored:Set[Position] = set() # used set instead of list due to O(1) when access the value 
+        # visited is mean the node that in the queue but might not explore yet
+        self.visited:Set[Position] = set() # used set instead of list due to O(1) when access the value
         self.m:int = 0 # row number of the map
         self.n:int = 0 # column number of the map
         self.len_ZT:int = 0 # number of Zanka no Tachi
         self.start:Position = (0, 0)  # start position (row, col)
         self.goal:Position = (0, 0)  # goal position (row, col)
+        self.cell_cache : Dict[Position, int] = {} # remember that is this state call explore function
+        self.best_distance : Dict[Position, int] = {} #closed list : with better distance only
+        self.zt_move_offsets: List[Position] = []
 
-    def successor(self, current_position:Position, action:Action)->Position|None:
-        dr, dc = action
+    def state_transition(self, current_position:Position, successor:Action)->Position|None:
+        dr, dc = successor
         next_position = (current_position[0] + dr, current_position[1] + dc)
 
         # the column and row should start at (1, 1) and end with -1 because the given size includes the wall
         # shotcut circuit due to that the posibility of the next position is explored higher than wall
-        if (next_position in self.explored) or \
-            (next_position[0] <= 0) or (next_position[1] <= 0) or \
+        if (next_position[0] <= 0) or (next_position[1] <= 0) or \
             (next_position[0] >= self.m - 1) or (next_position[1] >= self.n - 1):
             return None
         return next_position
@@ -75,7 +80,7 @@ class Search:
         self.start = (sr, sc)
         self.goal = (gr, gc) # this is for hueristic function in A* search
         self.fringe.append(self.start)
-        self.explored.add(self.start)
+        self.visited.add(self.start)
 
     def dfs(self):
         """
@@ -85,22 +90,22 @@ class Search:
 
         while len(self.fringe) > 0:
             position = self.fringe.pop(-1)
-            stack:List[Position] = list()
+            temp_stack:List[Position] = list()
             
             for action in self.SUCCESSORS:
-                next_position = self.successor(position, action)
+                next_position = self.state_transition(position, action)
 
-                if next_position:
-                    self.explored.add(next_position) # prevent duplicated explore calls, due to that the node in fringe will not explore again
-                    result = explore(next_position[0], next_position[1])
-                    if result =="G":
+                if next_position and (next_position not in self.visited):
+                    self.visited.add(next_position) # prevent duplicated explore calls, due to that the node in fringe will not explore again
+                    state = explore(next_position[0], next_position[1])
+                    if state =="G":
                         return
-                    elif result == "X":
+                    elif state == "X":
                         continue
                     else:
                         # Do not use insert because it take O(n)
-                        stack.append(next_position)
-            self.fringe.extend(stack[::-1])
+                        temp_stack.append(next_position)
+            self.fringe.extend(temp_stack[::-1])
 
     def initialize_bfs(self, m:int, n:int, sr:int, sc:int, gr:int, gc:int):
         """
@@ -112,7 +117,7 @@ class Search:
         self.start = (sr, sc)
         self.goal = (gr, gc)
         self.fringe.append(self.start)
-        self.explored.add(self.start)
+        self.visited.add(self.start)
 
     def bfs(self) -> None:
         """
@@ -123,38 +128,21 @@ class Search:
             position = self.fringe.pop(0)
             
             for action in self.SUCCESSORS:
-                next_position = self.successor(position, action)
-                if next_position:
-                    self.explored.add(next_position) # prevent duplicated explore calls, due to that the node in fringe will not explore again
-                    result=explore(next_position[0], next_position[1])
-                    if result =="G":
+                next_position = self.state_transition(position, action)
+                if next_position and (next_position not in self.visited):
+                    self.visited.add(next_position) # prevent duplicated explore calls, due to that the node in fringe will not explore again
+                    state=explore(next_position[0], next_position[1])
+                    if state =="G":
                         return
-                    elif result == "X":
+                    elif state == "X":
                         continue
                     else:
                         self.fringe.append(next_position)
 
-    @staticmethod
-    def _manhattan_distance(pos1:Position, pos2:Position) -> int: # because we can perform only 4 action
-        return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
-    # have to find new heuristic function that fit with ZT and without ZT due to manhattan distance is baseline
-    
     def heuristic(self, position:Position)->int:
-        # should we consider ZT used or not?
         dx = abs(position[0]-self.goal[0])
         dy = abs(position[1]- self.goal[1])
-        return dx + dy + min(dx, dy) - self.len_ZT
-    
-    def successor_Astar(self, current_position:Position, action:Action)->Position|None:
-        dr, dc = action
-        next_position = (current_position[0] + dr, current_position[1] + dc)
-
-        # the column and row should start at (1, 1) and end with -1 because the given size includes the wall
-        # shotcut circuit due to that the posibility of the next position is explored higher than wall
-        if  (next_position[0] <= 0) or (next_position[1] <= 0) or \
-            (next_position[0] >= self.m - 1) or (next_position[1] >= self.n - 1):
-            return None
-        return next_position
+        return (dx + dy)*2
 
     def initialize_Astar(self, m:int, n:int, sr:int, sc:int, gr:int, gc:int, k:int):
         """
@@ -167,20 +155,25 @@ class Search:
         self.start = (sr, sc)
         self.goal = (gr, gc)
 
-        self.successors: List[Position] = [
+        self.fringe : List[Node] = list() # open list
+        heapq.heappush(self.fringe, Node(self.start, 0, self.heuristic(self.start), False))
+
+        self.cell_cache : Dict[Position, int] = {self.start: 0} # remember that is this state call explore function
+        self.best_distance : Dict[Position, int] = {} #closed list : with better distance only
+        # self.visited is used to preventing from re-expanding the node that already expand
+
+        self.zt_move_offsets: List[Position] = [
             (dx * i, dy * i)
             for i in range(1, self.len_ZT + 1)
             for dx, dy in self.SUCCESSORS
         ]
 
-        # print(self.successors)
-
-        self.fringe : List[Node] = list() # open list
-        heapq.heappush(self.fringe, Node(self.start, 0, self.heuristic(self.start), False))
-
-        self.cell_cache : Dict[Position, str] = {self.start: "S"} # remember that is this state call explore function
-        self.best_distance : Dict[Position, int] = {} #closed list : with better distance only
-        self.position_cache : Set[Position] = set(self.start) # preventing from re-expanding the node that already expand
+        # self.heuristic_vals = {
+        #     (row, col): self.heuristic((row, col))
+        #     for row in range(1, m - 1)
+        #     for col in range(1, n - 1)
+        # }
+        # but it takes too much memory and computation in the large map: due to we didn't need to visited all cell in the map
 
     def Astar(self)->int:
         """
@@ -190,56 +183,53 @@ class Search:
 
         while len(self.fringe) > 0:
             node = heapq.heappop(self.fringe)
-            current_position = node.position 
+            node_key = node.get_key()
             if node.is_goal(self.goal):
                 return node.distance
                 #return node.distance, node
-            elif (current_position, node.is_ZT_used) in self.position_cache:
+            elif node_key in self.visited:
+                del(node) # prevent memory leak
                 continue
             else:
-                self.position_cache.add((current_position, node.is_ZT_used))
+                self.visited.add(node_key)
             # if node.position was pop from the queue then continue because it already pop the best node
             
             if node.is_ZT_used:
-                self.perform_action(self.SUCCESSORS, node, current_position, False, True)
+                self.expand_node(self.SUCCESSORS, node,  False, True)
             else:
-                self.perform_action(self.SUCCESSORS, node, current_position, False, False)
-                if self.len_ZT:
-                    self.perform_action(self.successors, node, current_position,True, True)
+                self.expand_node(self.SUCCESSORS, node,  False, False)
+                if self.len_ZT > 0:
+                    self.expand_node(self.zt_move_offsets, node, True, True)
 
 
-    def perform_action(self, successors, node, current_position, use_ZT, is_ZT_used):
+    def expand_node(self, successors:List[Action], node:Node, use_ZT:bool, is_ZT_used:bool):
         for successor in successors:
-            dr, dc = successor
-            next_position = (current_position[0] + dr, current_position[1] + dc)
+            next_position = self.state_transition(node.position, successor)
+            if  next_position:
+                if (next_position, is_ZT_used) in self.best_distance and node.distance >= self.best_distance[(next_position, is_ZT_used)]:
+                    continue   # worse path > reject, prevent from compute too much
 
-            if  not ((next_position[0] <= 0) or (next_position[1] <= 0) or \
-                (next_position[0] >= self.m - 1) or (next_position[1] >= self.n - 1)):
                 if use_ZT:
-                    state = "ZT"
+                    weight = 0
                 elif next_position in self.cell_cache:
                     # if it in cache, no need to explore again -> {(1,1):"S"} is a constant-time average operation due to the underlying hash table implementation which equal to set structure.
-                    state = self.cell_cache[next_position]
+                    weight = self.cell_cache[next_position]
                 else:
                     state:str=explore(next_position[0], next_position[1])
-                    self.cell_cache[next_position] = state
-                    
+                    if state == "X":
+                        weight = None
+                    elif state in {"S", "G"}:
+                        weight = 0
+                    else:
+                        weight = int(state)
+                    self.cell_cache[next_position] = weight
 
                 # Compute g(n)
-                if state == "X":
+                if weight is None:
                     continue
-                elif state in {"S", "G", "ZT"}:
-                    distance = node.distance
-                else:
-                    distance = node.distance + int(state)
-                
-                if (next_position, is_ZT_used) in self.best_distance and distance >= self.best_distance[(next_position, is_ZT_used)]:
-                    continue   # worse path > reject, prevent from compute too much
+                distance = node.distance + weight
                 self.best_distance[(next_position, is_ZT_used)] = distance
                 
-                heuristic_val = self.heuristic(next_position)
-                heapq.heappush(self.fringe, Node(next_position, distance, heuristic_val, is_ZT_used, node))
-
-    # TODO : refactor code
-    # TODO : change heuristic function
-    # TODO : optimize performance both time and space
+                heuristic_val = self.heuristic(next_position) 
+                # heapq.heappush(self.fringe, Node(next_position, distance, heuristic_val, is_ZT_used, node))
+                heapq.heappush(self.fringe, Node(next_position, distance, heuristic_val, is_ZT_used))
